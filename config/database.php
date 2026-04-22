fe7a3bd0a4d328168efeb452d7c9790bc98f2cfc
pi930 <t.pierrard.131.198@outlook.fr>
2026-04-22 22:05:29 +0200

    Fix pgsql config for DATABASE_URL

--- config/database.php
+++ config/database.php
@@ -87,17 +87,18 @@ return [
         'pgsql' => [
     'driver' => 'pgsql',
     'url' => env('DATABASE_URL'),
-    'host' => null,
-    'port' => null,
-    'database' => null,
-    'username' => null,
-    'password' => null,
+    'host' => env('DB_HOST'),
+    'port' => env('DB_PORT'),
+    'database' => env('DB_DATABASE'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
     'charset' => 'utf8',
     'prefix' => '',
     'prefix_indexes' => true,
     'search_path' => 'public',
     'sslmode' => 'require',
 ],
+
 
 
         'sqlsrv' => [
