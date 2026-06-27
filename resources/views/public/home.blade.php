e7c05510bd75e2b3f3ea85cc42965be02dc800bd
Thomas <pi930@users.noreply.github.com>
2026-06-27 08:50:31 +0200

    Changement couleur texte en bleu ciel

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -128,10 +128,11 @@
             Bienvenue chez Infortom
         </h1>
 
-        <p class="compact-text" style="font-size: 17px; color: #555;">
-            Votre partenaire informatique pour le dépannage, l’assistance, le développement web
-            et les solutions numériques adaptées à vos besoins.
-        </p>
+        <p class="compact-text" style="font-size: 17px; color: #87CEFA;">
+    Votre partenaire informatique pour le dépannage, l’assistance, le développement web
+    et les solutions numériques adaptées à vos besoins.
+</p>
+
 
         @auth
             <a href="{{ route('contact') }}"
