0cbe781f14a7fd0d0ecd2cc3b54402b130f91e7f
Thomas <pi930@users.noreply.github.com>
2026-07-14 08:19:23 +0200

    Add ENV CHECK debug lines

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+echo "ENV CHECK:"
+env | grep -i database
+env | grep -i db
 
 php artisan migrate --force || true
 
